c0cf57647d70d60c351fcdff2129ee91de8de74f
EstebanMD <esteban.md123@gmail.com>
2019-10-18 14:43:50 -0300

    Cambie la vista de ListarPedidos a ListarCrearPedidos

--- prestamo_materiales/urls.py
+++ prestamo_materiales/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.ListarPedidoView.as_view(), name='ListarPedidos'),
+	path('', views.ListarCrearPedidoView.as_view(), name='ListarPedidos'),
 
 ]
--- prestamo_materiales/views.py
+++ prestamo_materiales/views.py
@@ -1,12 +1,25 @@
-#
+from rest_framework import status #nose que es, pero lo importa para crear/listar cursos
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from . import models
 from . import serializers
 
-class ListarPedidoView(APIView):
+class ListarCrearPedidoView(APIView):
 	def get(self, request, format=None):
 		pedidos = models.Pedido.objects.all()
 		serializer = serializers.PedidoSerializer(pedidos, many=True)
 		return Response(serializer.data)
+
+	def post(self, request, format=None):
+		serializer = serializers.PedidoSerializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
+		
+
+
+
+
+
+
